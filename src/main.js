9846d3759d2bba7f83de61db525fcff4eaff9e22
Andrey <andreymail98@gmail.com>
2020-08-15 11:46:40 +0300

    add yandex map

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import YmapPlugin from 'vue-yandex-maps'
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+
+const settings = {
+  apiKey: '9aea7bb6-c605-4070-8e26-bb5de98573d6',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  version: '2.1'
+}
+
+Vue.use(BootstrapVue)
+Vue.use(YmapPlugin, settings)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <yandex-map
+      class="home-map"
+      :coords="coords"
+      :zoom="zoom"
+      :controls="controls"
+    >
+      <ymap-marker
+        v-for="marker in places"
+        :key="marker.id"
+      />
+    </yandex-map>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      coords: [54.817842, 58.446423],
+      zoom: 15,
+      controls: ["zoomControl"],
+      places: [],
+    }
   }
 }
 </script>
+
+<style lang="less">
+
+.home {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  z-index: 0;
+
+  &-map {
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
